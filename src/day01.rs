91d712fe71e0a6339c8b72b0ddcbdc5939eec04f
thuchede <huchede.thomas@gmail.com>
2023-12-02 17:26:37 +0800

    day01

---
+++ src/day01.rs
@@ -0,0 +1,93 @@
+use crate::helpers;
+
+// ____________________
+// Part 1
+// ____________________
+
+pub fn part_1() -> i64 {
+    read_from("src/input/day01.txt")
+}
+
+fn read_from(filepath: &str) -> i64 {
+    let sample = helpers::read(filepath);
+    sample.unwrap().iter().map(|line| get_coordinates(line)).reduce(|a, b| a+b).unwrap()
+}
+
+fn get_coordinates(amended_coordinates: &str) -> i64 {
+    let first = amended_coordinates.chars().find(|c| { c.is_numeric()});
+    let last = amended_coordinates.chars().rev().find(|c| { c.is_numeric()});
+    if let (Some(f), Some(l)) = (first, last) {
+        (f.to_string() + l.to_string().as_str()).parse().unwrap()
+    } else {
+        0
+    }
+}
+
+// ____________________
+// Part 2
+// ____________________
+
+pub fn part_2() -> i64 {
+    read_from_v2("src/input/day01.txt")
+}
+
+fn read_from_v2(filepath: &str) -> i64 {
+    let sample = helpers::read(filepath);
+    sample.unwrap().iter().map(|line| get_coordinates_v2(line)).reduce(|a, b| a+b).unwrap()
+}
+
+fn get_coordinates_v2(amended_coordinates_with_txt: &str) -> i64 {
+    // replace to easily match numbers
+    // dup text-based version so we don't strip a letter
+    // e.g. eightwo => e8t2o
+    let amended_coordinates = amended_coordinates_with_txt
+        .replace("one", "o1e")
+        .replace("two", "t2t")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
+    return get_coordinates(amended_coordinates.as_str())
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day1_1_part_1_sample() {
+        let res = read_from("src/input/sample01.txt");
+        assert_eq!(res, 142);
+    }
+
+    #[test]
+    fn test_day1_1_get_coordinates() {
+        let res1 = get_coordinates("abc12ds");
+        assert_eq!(res1, 12);
+        let res2 = get_coordinates("abc7ds");
+        assert_eq!(res2, 77);
+        let res3 = get_coordinates("ab1c23d4s");
+        assert_eq!(res3, 14);
+    }
+
+    #[test]
+    fn test_day1_2_get_coordinates_v2() {
+        let res1 = get_coordinates_v2("two1nine");
+        assert_eq!(res1, 29);
+        let res2 = get_coordinates_v2("eightwo");
+        assert_eq!(res2, 82);
+        let res3 = get_coordinates_v2("abcone2threexyz");
+        assert_eq!(res3, 13);
+    }
+
+    #[test]
+    fn test_day1_2() {
+        let res = part_2();
+        assert_eq!(res, 53221);
+    }
+}
---
+++ src/helpers.rs
@@ -0,0 +1,14 @@
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::result::Result;
+use std::fs::File;
+
+pub fn read(filepath: &str) -> Result<Vec<String>, Error> {
+    let file = match File::open(filepath) {
+        Ok(f) => f,
+        Err(_) => return Err(Error::new(ErrorKind::NotFound,format!("file {} does not exist!", filepath)))
+    };
+    let br = BufReader::new(file);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
